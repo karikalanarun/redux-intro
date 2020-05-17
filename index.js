25f6b60071177e6eda1bcec2e9b17840fea98bf1
Arun Karikalan <arun.digital.karikalan1994@gmail.com>
2020-05-17 14:52:42 +0530

    combineReducers

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const { createStore } = require("redux");
+const { createStore, combineReducers } = require("redux");
 // console.log("Hello world");
 
+// {
+//     incNumber: {number: 1},
+//     mulDivNumber: {number: 1} // actions MUL, DIV
+// }
+
 // incReducer :: currState -> action -> newState
-function incReducer(currState = { number1: 0, number2: 0 }, action) {
+function incReducer(currState = { number: 0 }, action) {
   switch (action.type) {
     case "INC":
       return { ...currState, number: currState.number + 1 };
@@ -13,13 +18,31 @@ function incReducer(currState = { number1: 0, number2: 0 }, action) {
   }
 }
 
+function mulDivReducer(currState = { number: 1 }, action) {
+  switch (action.type) {
+    case "MUL":
+      return { ...currState, number: currState.number * action.multiplier };
+    case "DIV":
+      return { ...currState, number: currState.number / action.divider };
+    case "INC":
+      return { ...currState, number: currState.number + 1 };
+    default:
+      return currState;
+  }
+}
+
+const appReducer = combineReducers({
+  incNumber: incReducer,
+  mulDivNumber: mulDivReducer,
+});
+
 // function add(a = 5, b) {
 //   return a + b;
 // }
 
 // console.log("add :: ", add(undefined, 4));
 
-const store = createStore(incReducer);
+const store = createStore(appReducer);
 
 console.log("store ::: currState :: ", store.getState());
 
@@ -27,6 +50,12 @@ store.subscribe((e) => {
   console.log("newState ::: ", store.getState());
 });
 
+function divAction(divider) {
+  return { type: "DIV", divider };
+}
+
 store.dispatch({ type: "INC" });
 store.dispatch({ type: "DEC" });
+store.dispatch({ type: "MUL", multiplier: 5 });
+store.dispatch(divAction(5));
 // store.dispatch({ type: "INC" });
